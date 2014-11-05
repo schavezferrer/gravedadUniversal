ca5c21193e8654214bd4975ba7d2db772e58377d
Sebas <atopesebas@msn.com>
2014-11-05 20:05:40 +0100

    no message

--- Assets/Scripts/Tutorial8.js
+++ Assets/Scripts/Tutorial8.js
@@ -36,11 +36,9 @@ function FixedUpdate () {
 		var col2 : Vector4 = obj.renderer.material.GetVector("_TintColor");
 		if (col2[3] < 92.0/255) col2[3] += Time.fixedDeltaTime*1.5;
 		obj.renderer.material.SetVector("_TintColor",col2);// -= Time.fixedDeltaTime;
-//		gameObject.transform.parent.FindChild("Dedo").position = Camera.main.ViewportToWorldPoint(Vector3(0.2,0.9,5));
-		obj.transform.FindChild("Dedo").active = true;
-		obj.transform.FindChild("Dedo").FindChild("Particle System").active = true;
-		obj.transform.FindChild("Dedo").FindChild("Texto").active = true;
-		obj.transform.FindChild("Dedo").FindChild("Texto2").active = true;
+
+		obj.transform.FindChild("Texto").active = true;
+		obj.transform.FindChild("Texto").transform.position = Camera.main.transform.position + Camera.main.transform.forward*2;
 	}
 	
 	if(VirtualInput.onClick() && mostrar && col2[3] > 92.0/255) desvanecer = true;
@@ -53,10 +51,10 @@ function FixedUpdate () {
 		col[3] -= Time.fixedDeltaTime*1.5;
 		obj.renderer.material.SetVector("_TintColor",col);// -= Time.fixedDeltaTime;
 		
-		obj.transform.FindChild("Dedo").active = false;
-		obj.transform.FindChild("Dedo").FindChild("Particle System").active = false;
-		obj.transform.FindChild("Dedo").FindChild("Texto").active = false;
-		obj.transform.FindChild("Dedo").FindChild("Texto2").active = false;
+//		obj.transform.FindChild("Dedo").active = false;
+//		obj.transform.FindChild("Dedo").FindChild("Particle System").active = false;
+		obj.transform.FindChild("Texto").active = false;
+//		obj.transform.FindChild("Dedo").FindChild("Texto2").active = false;
 		
 		if(timer < -1) GameObject.Destroy(gameObject.transform.parent.gameObject);
 	}
---
+++ Assets/Scripts/Tutorial9.js
@@ -0,0 +1,76 @@
+﻿#pragma strict
+
+var obj : GameObject;
+var target : GameObject;
+private var desvanecer : boolean;
+private var mostrar : boolean;
+private var timer : float;
+
+private var done : boolean;
+
+private var start  : boolean;
+
+function Start () {
+
+	timer = 1;
+
+}
+
+function FixedUpdate () {
+
+
+	if(!done)
+	{
+		if(start)
+		{
+			done = true;
+			
+			
+				
+//				gameObject.transform.parent.FindChild("Dedo").position = Camera.main.ViewportToWorldPoint(Vector3(0.2,0.9,5));
+//				gameObject.transform.parent.FindChild("Dedo").position.z = -5;
+//				gameObject.transform.parent.FindChild("Dedo").position.x += 3;
+//				gameObject.transform.parent.FindChild("Dedo").position.y += 1.5;
+				
+				mostrar = true;
+
+			(Camera.main.GetComponent(gameManager) as gameManager).setSwapEnabled(true);
+			
+		}
+	}
+
+	if(mostrar && !desvanecer)
+	{
+		var col2 : Vector4 = obj.renderer.material.GetVector("_TintColor");
+		if (col2[3] < 92.0/255) col2[3] += Time.fixedDeltaTime*1.5;
+		obj.renderer.material.SetVector("_TintColor",col2);// -= Time.fixedDeltaTime;
+//		gameObject.transform.parent.FindChild("Dedo").position = Camera.main.ViewportToWorldPoint(Vector3(0.2,0.9,5));
+		obj.transform.FindChild("Dedo").active = true;
+		obj.transform.FindChild("Dedo").FindChild("Particle System").active = true;
+		obj.transform.FindChild("Dedo").FindChild("Texto").active = true;
+	}
+	
+	if(VirtualInput.onClick() && mostrar && col2[3] > 92.0/255) desvanecer = true;
+
+	
+	if(desvanecer && mostrar)
+	{
+		timer -= Time.fixedDeltaTime*1.5;
+		var col : Vector4 = obj.renderer.material.GetVector("_TintColor");
+		col[3] -= Time.fixedDeltaTime*1.5;
+		obj.renderer.material.SetVector("_TintColor",col);// -= Time.fixedDeltaTime;
+		
+		obj.transform.FindChild("Dedo").active = false;
+		obj.transform.FindChild("Dedo").FindChild("Particle System").active = false;
+		obj.transform.FindChild("Dedo").FindChild("Texto").active = false;
+		
+		if(timer < -1) GameObject.Destroy(gameObject.transform.parent.gameObject);
+	}
+
+}
+
+function OnTriggerEnter(Col : Collider)
+{
+	if(Col.gameObject.CompareTag("Player")) start = true;
+}
+
